7c07797aea4e3c2a79f57372103d65b821668596
Jailani Abdul Rahman <jailani.rahman@pb.edu.bn>
2023-08-09 09:14:18 +0800

    First half of week03 slot 01

---
+++ week03/slot01/app/TestApp.java
@@ -0,0 +1,33 @@
+package week03.slot01.app;
+
+import week03.slot01.assets.Map;
+import week03.slot01.assets.Pirate;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		Map map = new Map(5, 5);
+		map.arena[3][3] = new Pirate("Abu", 4);
+		
+		Map anotherMap = new Map(7, 3);
+		
+		printArena(map);
+		
+		printArena(anotherMap);
+		
+		anotherMap.arena[2][0] = new Pirate("Bakar", 2);
+		
+		printArena(anotherMap);
+	}
+	
+	public static void printArena(Map map) {
+		for(int y = 0; y < map.arena[0].length; y++) {
+			for (int x = 0; x < map.arena.length; x++) {
+				System.out.print(map.arena[x][y] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println("****************");
+	}
+	
+}
---
+++ week03/slot01/assets/Map.java
@@ -0,0 +1,11 @@
+package week03.slot01.assets;
+
+public class Map {
+
+	public Pirate[][] arena;
+	
+	public Map(int width, int height) {
+		arena = new Pirate[width][height];
+	}
+	
+}
